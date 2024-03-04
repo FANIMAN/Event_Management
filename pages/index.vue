96663fb49e242d04da97a6cc779217ccff0efb80
FANIMAN <fantahunfekadu1@gmail.com>
2024-03-04 09:19:16 +0300

    add loadinfg state

--- pages/index.vue
+++ pages/index.vue
@@ -54,7 +54,7 @@
                             <p>መጋቢት አንድ ቀን ፥በእለተ እሁድ ፥ በሚሊኒየም አዳራሽ "እድል ነው" የተሰኘ ያዝናኝ ወጎችና የጃዝ ድግስ ተሰናድቶ ይጠብቅዎታል! </p>
                         </div>
                         <div class="text-justify text-[15px] md:text-lg" v-else-if="activeTab === 'eventLocation'">
-                            <p>ቦሌ ጃፓን</p>
+                            <p>ሚሊኒየም አዳራሽ</p>
                         </div>
                         <div class="text-justify text-[15px] md:text-lg" v-else-if="activeTab === 'contact'">
                             <div class="flex w-full my-2">
@@ -216,7 +216,23 @@
                     <!-- Pay now Button -->
                     <button @click="pay" :disabled="!isValidPhoneNumber"
                         class="bg-[#D3AF35] hover:bg-[#D3AF39] text-white font-bold py-3 px-4 text-xl rounded mt-10 md:mt-0 mb-10 md:mb-0">
-                        Pay now
+                        <!-- Pay now -->
+                        <div v-if="isLoading" class="flex justify-center items-center gap-2">
+                            <svg aria-hidden="true"
+                                class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white dark:fill-gray-300"
+                                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill" />
+                            </svg>
+                            Processing...
+                        </div>
+                        <span v-else>
+                            Pay now
+                        </span>
 
                     </button>
 
@@ -234,7 +250,8 @@
             <div class="absolute inset-0 bg-black opacity-50"></div>
 
             <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow-md w-full md:w-4/5 p-6 py-32 overflow-y-auto h-full md:h-[700px]">
+            <div
+                class="relative bg-white rounded-lg shadow-md w-full md:w-4/5 p-6 py-32 overflow-y-auto h-full md:h-[700px]">
                 <button @click="closeModal" class="absolute top-3 right-3 text-gray-600 hover:text-gray-900">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
@@ -243,9 +260,10 @@
                     </svg>
                 </button>
 
-            <!-- iframe to display payment link -->
-        <!-- <iframe :src="paymentLink" class="w-full h-full"></iframe> -->
-        <iframe :src="paymentLink" class="w-full h-full iframe-no-scroll" style="overflow: hidden; border: none;" ></iframe>
+                <!-- iframe to display payment link -->
+                <!-- <iframe :src="paymentLink" class="w-full h-full"></iframe> -->
+                <iframe :src="paymentLink" class="w-full h-full iframe-no-scroll"
+                    style="overflow: hidden; border: none;"></iframe>
 
 
 
@@ -282,6 +300,7 @@ const phoneNumber = ref('');
 const isValidPhoneNumber = ref(false);
 const showInitiatePaymentModal = ref(false);
 const paymentLink = ref(null);
+const isLoading = ref(false);
 
 const showErrorMessage = ref(false);
 const errorMessage = ref('Tickets will be available starting from tomorrow');
@@ -313,6 +332,7 @@ const calculateDaysLeft = () => {
 
 const pay = async () => {
     // showErrorMessage.value = true; // Show error message
+    isLoading.value = true
 
     const data = {
         ticket_id: ticketPrice.value === 400 ? '245f618a-db8a-4b55-a707-67b83a29086e' : '402085c1-05d4-4779-b95a-2c51e068b51d',
@@ -332,12 +352,15 @@ const pay = async () => {
                 // Open payment link in a new tab
                 // window.open(paymentLink.value, '_blank');
 
-                 // Redirect to payment link in the same tab
-                 window.location.href = paymentLink.value;
+                // Redirect to payment link in the same tab
+                window.location.href = paymentLink.value;
+                isLoading.value = false
+
             }
         })
         .catch(error => {
             console.error('Error fetching event details:', error);
+            isLoading.value = false
         });
 
 
@@ -446,6 +469,7 @@ const amount = computed(() => {
 .valid {
     border: 1px solid green;
 }
+
 .iframe-no-scroll {
     overflow: hidden;
 }
